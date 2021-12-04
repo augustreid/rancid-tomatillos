7aafcad1878ef156ea0cb8f1efdbab9f3a05ca20
August Reid <c.august.reid@gmail.com>
2021-12-04 15:56:08 -0700

    Add conditional className logic to render different card colors based on rating

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -2,8 +2,21 @@ import React from 'react';
 import './MovieCard.css';
 
 function MovieCard({title,posterPath,averageRating}) {
+  let cardColor;
+  if (averageRating < 4) {
+    cardColor = "red-card";
+  } else if (averageRating < 5) {
+    cardColor = "orange-card";
+  } else if (averageRating < 6) {
+    cardColor = "blue-card";
+  } else if (averageRating < 7) {
+    cardColor = "green-card";
+  } else {
+    cardColor = "yellow-card";
+  }
+
   return (
-    <div className="movie-card">
+    <div className={cardColor}>
       <h3>{title}</h3>
       <img src={posterPath} alt= "poster image of movie" className="card-poster"/>
       <h5>{averageRating.toFixed(1)}⭐</h5>
