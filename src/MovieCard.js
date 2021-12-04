923054fb3c7b1ca7c80a6a8ce5eb27ba9fff6902
August Reid <c.august.reid@gmail.com>
2021-12-03 18:55:41 -0700

    Create grid display and sizing for movie cards

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -3,9 +3,9 @@ import './MovieCard.css';
 
 function MovieCard({title,posterPath,averageRating}) {
   return (
-    <div>
+    <div className="movie-card">
       <h3>{title}</h3>
-      <img src={posterPath} alt= "poster image of movie"/>
+      <img src={posterPath} alt= "poster image of movie" className="card-poster"/>
       <h5>{averageRating.toFixed(1)}⭐</h5>
     </div>
   )
