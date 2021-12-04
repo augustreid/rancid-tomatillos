ffdbb1660fdd7907fe86e0a92639dfbe2b1e067a
Tanya Zhuge <tangnazhuge@gmail.com>
2021-12-03 16:45:14 -0800

    Add App Class

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import movieData from './movieData';
 import './App.css';
+import Header from './Header';
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: movieData.movies
+    }
+  }
+
   return (
-   
+    <main>
+   <Header/>
+   <Movies movieInfo={this.state.movies}/>
+   <Details/> 
+   </main>
   );
 }
 
