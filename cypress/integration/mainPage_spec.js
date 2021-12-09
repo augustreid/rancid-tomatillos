4a9521128b86e81cf437f846898cb307fe71c896
August Reid <c.august.reid@gmail.com>
2021-12-09 13:59:25 -0700

    Add GET intercept for main page

--- cypress/integration/mainPage_spec.js
+++ cypress/integration/mainPage_spec.js
@@ -1,5 +1,41 @@
 describe('Rancid Tomatillos main page display', () => {
-    it('Should confirm that true is equal to true', () => {
-      expect(true).to.equal(true)
+
+    beforeEach(() => {
+        cy.visit("http://localhost:3000")
+    });
+
+    it('Should be able to visit the page and render the correct elements"', () => {
+        cy.intercept("GET", "http://localhost:3000", {
+             statusCode: 200,    
+             body: {
+                "movies": [
+                  {
+                    "id": 694919,
+                    "poster_path": "https://image.tmdb.org/t/p/original//6CoRTJTmijhBLJTUNoVSUNxZMEI.jpg",
+                    "backdrop_path": "https://image.tmdb.org/t/p/original//pq0JSpwyT2URytdFG0euztQPAyR.jpg",
+                    "title": "Money Plane",
+                    "average_rating": 6.666666666666667,
+                    "release_date": "2020-09-29"
+                  },
+                  {
+                    "id": 337401,
+                    "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+                    "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
+                    "title": "Mulan",
+                    "average_rating": 4.909090909090909,
+                    "release_date": "2020-09-04"
+                  },
+                  {
+                    "id": 718444,
+                    "poster_path": "https://image.tmdb.org/t/p/original//uOw5JD8IlD546feZ6oxbIjvN66P.jpg",
+                    "backdrop_path": "https://image.tmdb.org/t/p/original//x4UkhIQuHIJyeeOTdcbZ3t3gBSa.jpg",
+                    "title": "Rogue",
+                    "average_rating": 5.428571428571429,
+                    "release_date": "2020-08-20"
+                  }]
+            }
+      }) 
+        .get("main")
+        .contains("Rancid Tomatillos")
     });
   });
