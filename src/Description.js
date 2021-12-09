507d0ba2e0bde9d8d3b4926fff23a6f4bb100810
Tanya Zhuge <tangnazhuge@gmail.com>
2021-12-08 18:50:05 -0800

    Update Description function component to class component

--- src/Description.js
+++ src/Description.js
@@ -1,15 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Description.css';
+import Poster from './Poster';
+import Button from './Button';
 
-function Description({title, rating, summary, releaseDate}) {
-  return (
-    <div>
-    <h2>{title}</h2>
-    <p>{releaseDate}</p>
-    <p>{summary}</p>
-    <h3>{rating.toFixed(1)}  ⭐️</h3>
-    </div>
-  )
+class Description extends Component {
+  constructor(){
+    super()
+    this.state = {
+      loading: true,
+      singleMovie: null,
+    }
+  }
+
+  componentDidMount = () => {
+      fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${this.props.currentFilmId}`)
+      .then(response => response.json())
+      .then(data => this.setState({singleMovie: data.movie, loading: false}))
+      .then(this.render())
+      .catch(error => this.setState({error: true}))  
+    }
+  
+  backToMain = () => {
+      this.setState({singleMovie: null})
+    }
+
+  render() {
+    return (
+      <>
+      {this.state.loading ? <p>Loading Be Patient!</p> :
+      <div>
+        <h2>{this.state.singleMovie.title}</h2>
+        <p>{this.state.singleMovie.release_date}</p>
+        <p>{this.state.singleMovie.overview}</p>
+        <h3>{this.state.singleMovie.average_rating.toFixed(1)}  ⭐️</h3>
+      </div>
+      }
+      </>
+    )
+  }
 }
 
 export default Description;
