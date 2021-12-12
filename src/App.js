1bb121adf51aac2b1826a34a8ec127e45e5a7fd5
August Reid <c.august.reid@gmail.com>
2021-12-11 19:41:03 -0700

    Add Genre component and fetch the detailed data to hold in App state

--- src/App.js
+++ src/App.js
@@ -4,30 +4,64 @@ import './App.css';
 import Header from './Header';
 import Movies from './Movies';
 import Detail from './Detail';
+import Genre from "./Genre"
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       movies: [],
-      error: false
+      filmsInfo: null,
+      error: false,
+      loading: true
     }
   }
 
   componentDidMount = () => {
     fetch("https://rancid-tomatillos.herokuapp.com/api/v2/movies")
       .then(response => response.json())
-      .then(data => this.setState({movies: data.movies}))
+      .then(data => this.setState({movies: data.movies, loading: false}))
       .catch(error => this.setState({error: true}))
   }
+
+   getDetailData = () => {
+    const moviesId = this.state.movies.map((movie)=> {
+      return movie.id;
+    })
+    const movieDetails = []
+        moviesId.forEach((id) => {
+        fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`)
+        .then(response => response.json()) 
+        .then(data => movieDetails.push(data))
+      })
+    this.setState({filmsInfo: movieDetails})
+  }
+  
+  // getDetailData = () => {
+  //   const moviesId = this.state.movies.map((movie)=> {
+  //     return movie.id;
+  //   })
+  //   const allMovies = [];
+  //   console.log("moviesId", moviesId)
+  //   for (let i = 0; i < moviesId.length; i++) {
+  //     setTimeout(() => {fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${moviesId[i]}`)
+  //     .then(response => response.json()) 
+  //     .then(data => allMovies.push(data))
+  //     }, 500)
+  //   }
+  //   this.setState({ filmsInfo: allMovies })
+  // }
+
 
   render() {
     return (
       <main>
         <Header/>
+        {!this.state.loading && <Genre moviesInfo={this.state.movies} getDetailData={this.getDetailData}/>}
         <Routes>
           <Route path="/" element={<Movies moviesInfo={this.state.movies} />}/>
           <Route path="/:id"  element={<Detail moviesInfo={this.state.movies} />}/>
+          {/* <Route path="/:genre" element={<Genre moviesInfo={this.state.movies}/>}/> */}
         </Routes>
       </main>
     )
@@ -35,3 +69,4 @@ class App extends Component {
 }
 
 export default App;
+
---
+++ src/Genre.js
@@ -0,0 +1,47 @@
+import React, {Component} from "react"
+
+function Genre({moviesInfo, getDetailData}) {
+   setTimeout(() => {
+       getDetailData()
+   }, 500);
+   
+    return (
+        <p>genres</p>
+    )
+}
+
+// class Genre extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             detailData: null
+//         }
+//     }
+
+//     componentDidMount = () => {
+//         this.getDetailData();
+//     }
+
+//     getDetailData = () => {
+//         const moviesId = this.props.movieInfo.map((movie)=> {
+//           return movie.id;
+//         })
+//         const allMovies = [];
+//         for (let i = 0; i < moviesId.length; i++) {
+//           setTimeout(() => {fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${moviesId[i]}`)
+//           .then(response => response.json()) 
+//           .then(data => allMovies.push(data))
+//           }, 100)
+//         }
+//         this.setState({ detailData: allMovies })
+//       }
+
+//       render() {
+//           return (
+//               <h1>testing</h1>
+//           )
+//       }
+//     }
+
+
+export default Genre;
